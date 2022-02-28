0175a2ddb6b326ac79fb5634f8cc2338ef0da115
mallevondoning <malcolm.bergander@futuregames.nu>
2022-02-28 16:46:49 +0100

    added base stuff to the scene and basic scripts

---
+++ snake_assignment/Assets/Scripts/Enums.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    NoneID = 0,
+    up = 1,
+    left = 2,
+    down = 3,
+    right = 4,
+}
---
+++ snake_assignment/Assets/Scripts/snakeScript/HeadScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
